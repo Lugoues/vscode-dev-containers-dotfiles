d168e6bc740803edabed57d3ec3c79a440474106
Peter Brunner <peter@lugoues.net>
2021-02-06 17:23:57 -0500

    initial install.zsh

---
+++ install.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "Creating directories"
+mkdir -p $HOME/.local/{bin,share,etc}
+
+echo "Install piu"
+curl -s https://raw.githubusercontent.com/keithieopia/piu/master/piu -o ~/.local/bin/piu
+chmod +x ~/.local/bin/piu
+
+echo "Install required packages"
+export DEBIAN_FRONTEND=noninteractive
+piu i -y zsh neovim gawk subversion
+
+echo "Installing Ellipsis"
+export ELLIPSIS_PATH=$HOME/.local/share/ellipsis
+git clone git@github.com:ellipsis/ellipsis.git $HOME/.local/share/ellipsis
+ln -s $ELLIPSIS_PATH/bin/ellipsis $HOME/.local/bin/
+
+echo "Installing dotfiles"
+ellipsis install lugoues/zsh
+ellipsis install lugoues/nvim
+
